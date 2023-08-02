374a0459fc602d95d6a210c4dd357a4a5c6f34e9
DetectiveJeremy <jeremy.r.herrera.ortiz@gmail.com>
2023-08-02 10:45:40 -0400

    first

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 /* @TODO Get References */
+const pokemonInfoDiv = document.getElementById("pokemon_info")
+const screenImage = document.querySelector("img");
+const submitButton = document.getElementById("Submit-Button");
+const pokemonDropDown = document.getElementById("pokemon_dropdown")
+ 
+const changePokemon = ("click", e => {
+e.preventDefault();
+const userChoice = pokemonDropDown.value;
+console.log(userChoice)
+screenImage.src = "images/" + userChoice.toLowerCase() + ".png";
 
+pokemonInfoDiv.innerHTML = "It's" + userChoice + "!"
+});
 /* @TODO Event Handler */
-
+pokemonDropDown.addEventListener("change",changePokemon);
 /*  
     try updating when button is clicked vs when
     dropdown value changes with
     menu.onchange = 
 */
 
+
+submitButton.addEventListener("click",() => {
+   
+    changePokemon();
+});
 // prevent the page from reloading
 
 // capture userChoice
